33294c86e79f9d04134364c766e20e7a3dfeaa6a
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-04 16:12:25 -0500

    programmed traffic lights

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: March 4
+ * This program operates traffic lights
 */
 
-basic.showString('Hello, World!')
+// the variables 
+let myNeopixelStrip: neopixel.Strip = null
+
+// setting up the lights
+basic.clearScreen()
+myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// running the traffic lights
+basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+    myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    myNeopixelStrip.show()
+    basic.pause(1000)
+    myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    myNeopixelStrip.show()
+    basic.pause(1000)
+    myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    myNeopixelStrip.show()
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
+
+// resetting the traffic lights
+basic.clearScreen()
+input.onButtonPressed(Button.B, function () {
+myNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+myNeopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+})

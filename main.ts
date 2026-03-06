195ffdb79e2cdb59cd826b4c2691dd385cc5a028
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-03-06 12:58:46 -0500

    changed comments

--- main.ts
+++ main.ts
@@ -22,19 +22,31 @@ basic.showIcon(IconNames.Happy)
 basic.clearScreen()
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Square)
+
+    // turning on green traffic light
     myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     myNeopixelStrip.show()
     basic.pause(2000)
+
+    // turning off green traffic light
     myNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     myNeopixelStrip.show()
+
+    // turning on yellow light
     myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
     myNeopixelStrip.show()
     basic.pause(2000)
+
+    // turning off yellow light
     myNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     myNeopixelStrip.show()
+
+    // turning on red light
     myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
     myNeopixelStrip.show()
     basic.pause(2000)
+
+    // turning off red light
     myNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     myNeopixelStrip.show()
     basic.showIcon(IconNames.Happy)
